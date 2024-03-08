1d9923641a45196f2590bdded0d45420c942fa45
JorgeSintes <jorge.sintes@gmail.com>
2024-03-08 13:05:44 +0100

    Changed the Note class to have absPosition as its internal representation

--- src/core/notes.ts
+++ src/core/notes.ts
@@ -36,24 +36,29 @@ function isValidNoteName(noteName: string): boolean {
     return true;
 }
 
-function noteToInt(note: string): number {
+function noteToAbsPosition(note: string, octave: number): number {
     if (!isValidNoteName(note)) {
         throw new Error(`Invalid note name: ${note}`);
     }
     if (!noteNamesF.includes(note) && !noteNamesS.includes(note)) {
-        note = reduceAccidentals(note);
+        let reduced = reduceAccidentals(note, octave);
+        note = reduced.note;
+        octave = reduced.octave;
     }
     if (noteNamesF.includes(note)) {
-        return noteNamesF.indexOf(note);
+        var position = noteNamesF.indexOf(note);
+    } else {
+        var position = noteNamesS.indexOf(note);
     }
-    return noteNamesS.indexOf(note);
+
+    return position + octave * 12;
 }
 
-function reduceAccidentals(note: string): string {
+function reduceAccidentals(note: string, octave: number): { note: string; octave: number } {
     if (!isValidNoteName(note)) {
         throw new Error(`Invalid note name: ${note}`);
     }
-    let noteVal = noteToInt(note[0]);
+    let noteAbsPosition = noteToAbsPosition(note[0], octave);
     let accidentVal = 0;
     for (const token of note.slice(1)) {
         if (token === "#") {
@@ -63,31 +68,45 @@ function reduceAccidentals(note: string): string {
         }
     }
     if (accidentVal < 0) {
-        return noteNamesF[(noteVal + accidentVal) % 12];
+        return {
+            note: noteNamesF[(noteAbsPosition + accidentVal) % 12],
+            octave: Math.floor((noteAbsPosition + accidentVal) / 12),
+        };
     }
-    return noteNamesF[(noteVal + accidentVal) % 12];
+    return {
+        note: noteNamesS[(noteAbsPosition + accidentVal) % 12],
+        octave: Math.floor((noteAbsPosition + accidentVal) / 12),
+    };
 }
 
 class Note {
     name: string;
     position: number;
     octave: number;
+    absPosition: number;
     constructor(name: string, octave: number) {
         if (!isValidNoteName(name)) {
             throw new Error(`Invalid note name: ${name}`);
         }
         this.name = name;
-        this.position = noteToInt(this.name);
         this.octave = octave;
+        this.absPosition = noteToAbsPosition(this.name, this.octave);
+        this.position = this.absPosition % 12;
     }
-    static fromInt(position: number, octave: number, accidental: string = "#"): Note {
-        if (accidental !== "#" && accidental !== "b") {
-            throw new Error(`Invalid accidental: ${accidental}`);
+    static fromAbsPosition(note_name: string, absPosition: number) {
+        var pos = noteToAbsPosition(note_name, 0);
+        var offset = (absPosition - pos) % 12;
+        if (offset <= 6) {
+            var octave = Math.floor((absPosition - pos) / 12);
+        } else {
+            var octave = Math.floor((absPosition - pos) / 12) + 1;
+            offset = offset - 12;
         }
-        if (accidental === "#") {
-            return new Note(noteNamesS[position], octave);
+        if (offset >= 0) {
+            return new Note(note_name + "#".repeat(offset), octave);
+        } else {
+            return new Note(note_name + "b".repeat(Math.abs(offset)), octave);
         }
-        return new Note(noteNamesF[position], octave);
     }
     toInt(): number {
         return this.position;
@@ -116,15 +135,12 @@ class Note {
         return this.name === other.name;
     }
     distanceTo(other: Note): number {
-        // assumes other is a higher note than this
-        if (this.position > other.position) {
-            return other.position - this.position + 12;
-        }
-        return other.position - this.position;
+        return other.absPosition - this.absPosition;
     }
 
     reduceAccidentals(): Note {
-        return new Note(reduceAccidentals(this.name), this.octave);
+        let reduced = reduceAccidentals(this.name, this.octave);
+        return new Note(reduced.note, reduced.octave);
     }
 
     enharmonicEquivalent(): Note {
@@ -138,100 +154,100 @@ class Note {
         if (!this.name.endsWith("b")) {
             return new Note(this.name + "#", this.octave);
         }
-        return new Note(reduceAccidentals(this.name.slice(0, -1)), this.octave);
+        let reduced = reduceAccidentals(this.name.slice(0, -1), this.octave);
+        return new Note(reduced.note, reduced.octave);
     }
     diminish(): Note {
         if (!this.name.endsWith("#")) {
             return new Note(this.name + "b", this.octave);
         }
-        return new Note(reduceAccidentals(this.name.slice(0, -1)), this.octave);
+        let reduced = reduceAccidentals(this.name.slice(0, -1), this.octave);
+        return new Note(reduced.note, reduced.octave);
     }
     halfstepUp(): Note {
-        let next_position = ((this.position % 12) + 12) % 12;
-        let nextOctave = next_position > this.position ? this.octave : this.octave + 1;
-        return Note.fromInt((this.position + 1) % 12, nextOctave, "#");
+        let newNote = this.augment();
+        return newNote.reduceAccidentals();
     }
     halfstepDown(): Note {
-        let next_position = ((this.position % 12) + 12) % 12;
-        let nextOctave = next_position < this.position ? this.octave : this.octave - 1;
-        return Note.fromInt((this.position - 1) % 12, nextOctave, "b");
+        let newNote = this.diminish();
+        return newNote.reduceAccidentals();
     }
 
     // Intervals
     minorSecond(): Note {
         let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
         let nextNoteIdx = (thisNoteIdx + 1) % 7;
-        let nextOctave = nextNoteIdx > thisNoteIdx ? this.octave : this.octave + 1;
-        let nextNote = new Note(naturalNoteNames[nextNoteIdx], nextOctave);
-        let diff = this.distanceTo(nextNote) - 1;
-        if (diff < 0) {
-            nextNote = new Note(nextNote.name + "#".repeat(Math.abs(diff)), nextOctave);
-        } else if (diff > 0) {
-            nextNote = new Note(nextNote.name + "b".repeat(diff), nextOctave);
-        }
-        return nextNote;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 1);
     }
     majorSecond(): Note {
         let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
         let nextNoteIdx = (thisNoteIdx + 1) % 7;
-        let nextOctave = nextNoteIdx > thisNoteIdx ? this.octave : this.octave + 1;
-        let nextNote = new Note(naturalNoteNames[nextNoteIdx], nextOctave);
-        let diff = this.distanceTo(nextNote) - 2;
-        if (diff < 0) {
-            nextNote = new Note(nextNote.name + "#".repeat(Math.abs(diff)), nextOctave);
-        } else if (diff > 0) {
-            nextNote = new Note(nextNote.name + "b".repeat(diff), nextOctave);
-        }
-        return nextNote;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 2);
     }
     augmentedSecond(): Note {
         let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
         let nextNoteIdx = (thisNoteIdx + 1) % 7;
-        let nextOctave = nextNoteIdx > thisNoteIdx ? this.octave : this.octave + 1;
-        let nextNote = new Note(naturalNoteNames[nextNoteIdx], nextOctave);
-        let diff = this.distanceTo(nextNote) - 3;
-        if (diff < 0) {
-            nextNote = new Note(nextNote.name + "#".repeat(Math.abs(diff)), nextOctave);
-        } else if (diff > 0) {
-            nextNote = new Note(nextNote.name + "b".repeat(diff), nextOctave);
-        }
-        return nextNote;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 3);
     }
     minorThird(): Note {
-        return this.majorSecond().minorSecond();
+        let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
+        let nextNoteIdx = (thisNoteIdx + 2) % 7;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 3);
     }
     majorThird(): Note {
-        return this.majorSecond().majorSecond();
+        let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
+        let nextNoteIdx = (thisNoteIdx + 2) % 7;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 4);
     }
     diminishedForth(): Note {
-        return this.minorThird().minorSecond();
+        let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
+        let nextNoteIdx = (thisNoteIdx + 3) % 7;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 4);
     }
     perfectForth(): Note {
-        return this.majorThird().minorSecond();
+        let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
+        let nextNoteIdx = (thisNoteIdx + 3) % 7;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 5);
     }
     augmentedForth(): Note {
-        return this.majorThird().majorSecond();
+        let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
+        let nextNoteIdx = (thisNoteIdx + 3) % 7;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 6);
     }
     diminishedFifth(): Note {
-        return this.perfectForth().minorSecond();
+        let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
+        let nextNoteIdx = (thisNoteIdx + 4) % 7;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 6);
     }
     perfectFifth(): Note {
-        return this.perfectForth().majorSecond();
+        let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
+        let nextNoteIdx = (thisNoteIdx + 4) % 7;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 7);
     }
     augmentedFifth(): Note {
-        return this.augmentedForth().majorSecond();
+        let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
+        let nextNoteIdx = (thisNoteIdx + 4) % 7;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 8);
     }
     minorSixth(): Note {
-        return this.perfectFifth().minorSecond();
+        let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
+        let nextNoteIdx = (thisNoteIdx + 5) % 7;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 8);
     }
     majorSixth(): Note {
-        return this.perfectFifth().majorSecond();
+        let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
+        let nextNoteIdx = (thisNoteIdx + 5) % 7;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 9);
     }
     minorSeventh(): Note {
-        return this.majorSixth().minorSecond();
+        let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
+        let nextNoteIdx = (thisNoteIdx + 6) % 7;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 10);
     }
     majorSeventh(): Note {
-        return this.majorSixth().majorSecond();
+        let thisNoteIdx = naturalNoteNames.indexOf(this.name[0]);
+        let nextNoteIdx = (thisNoteIdx + 6) % 7;
+        return Note.fromAbsPosition(naturalNoteNames[nextNoteIdx], this.absPosition + 11);
     }
 }
 
